c77bd20af0aa582b83cf13166efe73a1d47b016d
miekale <miekale.smith@gmail.com>
2025-04-18 15:43:32 -0400

    new project automation

--- new_project.py
+++ new_project.py
@@ -4,6 +4,84 @@ import shutil
 template_path = "projects/template.html"
 
 name = input("Enter the name of the new project: ")
+folder_name = name.replace(" ", "_")
 
-os.mkdir(f"media/{name}")
-shutil.copy(template_path,f"projects/{name}.html")
+try:
+    os.mkdir(f"media/projects/{name}")
+
+except FileExistsError:
+    input("delete folder y/n:")
+    if input() == "y": 
+        shutil.rmtree(f"projects/{folder_name}")
+        os.mkdir(f"media/projects/{name}")
+    else:
+        print("Using existing folder")
+
+input("Enter when finished puttting images in the media/projects/{folder_name} folder")
+
+# Create the HTML file
+slide_index = "".join([f'\n                   <span class="dot" onclick="currentSlide({num})"></span>' 
+                       for num in range(1, len(os.listdir(f"media/projects/{folder_name}")) + 1)])
+
+slideshow = "".join([f'''
+            <div class="slide">
+                <img src="../media/projects/{folder_name}/{img}">
+            </div>\n''' 
+            for img in os.listdir(f"media/projects/{folder_name}")
+        ])
+
+file =f'''
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../Style.css">
+        
+        <title>Miekale Smith's Portfoilo</title>
+    
+        <div class="nav">
+            <ol>
+                <li><a href="../Home.html#home">HOME</a></li>
+                <li><a href="../Home.html#projects">PROJECTS</a></li>
+                <li><a href="../Home.html#connect">CONNECT</a></li>
+            </ol>
+        </div>
+
+    </head>
+
+    <body style="background-color: #2A2C36;">
+        <div class="project-pages">
+            <h1>{name}</h1>
+            
+            <div class="slideshow">
+
+            {slideshow}
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                <div style="text-align:center">
+                {slide_index}
+                </div>
+            </div>
+            
+            <div class="project-pages-main">
+                <!-- Main Bullet points --> 
+                <ul>
+                    <li></li>
+                    <li></li>
+                </ul>   
+                <!-- Description Paragraphs --> 
+                <p>
+                </p>
+            </div>
+            <div class="back-button">
+                <a href="../Home.html#projects">BACK TO PROJECTS</a>
+            </div>
+            <script src="../scripts.js"></script>
+        </div>
+    </body>
+</html>
+'''
+
+with open(f"projects/{folder_name}.html", "w") as f:
+    f.write(file)
